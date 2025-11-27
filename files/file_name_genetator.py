ce11e9f7b7746f58054c836186054c1d816152a8
Edgar <edgar.nashville@gmail.com>
2025-11-27 10:29:32 -0500

    better file name gen

--- files/file_name_genetator.py
+++ files/file_name_genetator.py
@@ -12,9 +12,10 @@ date_format ="%Y-%m-%d_%l:%M%p"
 formatted_string = now.strftime(date_format)
 print(formatted_string)
 
-file_path = 'formatted_string'
+file_path = formatted_string
+location = "_MA"
+word = "_word_of_the_day"
+print(formatted_string)
 
-print(str(formatted_string))
-
-with open(file_path + ".txt", "w") as file:
+with open(file_path + location + word + ".txt", "w") as file:
     file.write("Your text goes here")
